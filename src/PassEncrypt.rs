b2cb36a0f991aced2d7f965089ad3ba5e112d5e0
foxjazz <fox21@foxjazz.net>
2022-12-16 07:22:43 -0700

    second using trait

--- src/PassEncrypt.rs
+++ src/PassEncrypt.rs
@@ -5,66 +5,52 @@ use aes::cipher::{
     generic_array::GenericArray,
 };
 
-pub struct Pass_Encrypt {
-
+trait Encrypt{
+    fn encrypt(&self, password : String) -> String;
 }
-/*fn generate_hash(password: &str) -> Vec<u8> {
-    // Create a new hasher
-    let mut hasher = DefaultHasher::new();
+trait Decrypt{
+    fn decrypt(&self, password: String) -> Result<String,E>;
+}
 
-    // Hash the password and retrieve the resulting hash value
-    password.hash(&mut hasher);
-    let hash = hasher.finish();
-
-    // Convert the hash value to a vector of bytes
-    let mut hash_bytes: Vec<u8> = Vec::new();
-    for b in hash.to_le_bytes().iter() {
-        hash_bytes.push(*b);
-    }
-
-    // Return the vector of bytes
-    hash_bytes
-}*/
-impl Pass_Encrypt{
-    /*pub fn new (password: &str) -> Pass_Encrypt {
-        //let mut hasher = Sha256::digest::new();
-//        let key = generate_hash(password) ;
-        let key = Sha256::digest(password.as_bytes());
-        let iv = [0; 16];
-        Pass_Encrypt {key, iv}
-    }*/
-
-    fn get_key(password: String) -> String {
-
-        let mut pw = password.clone();
-        while (pw.len() < 16){
-            pw += password.as_str();
-        }
-            pw
-    }
-    pub fn enc( plain_text : String, password: String) -> String {
+impl Encrypt for String{
+    fn encrypt( &self, password: String) -> String {
         let key_string = Pass_Encrypt::get_key(password);
         let key = GenericArray::clone_from_slice(key_string.as_bytes());
         let cipher = Aes128::new(&key);
-        let mut block = GenericArray::clone_from_slice(plain_text.as_bytes());
+        let mut block = GenericArray::clone_from_slice((plain_text + "verify".to_string()).as_bytes());
 
         cipher.encrypt_block(&mut block);
         let s = block.to_vec();
         std::str::from_utf8(&s).unwrap().to_string()
-
     }
-   /* pub fn dec(&self, cipher_text : String) -> Result<String,ParseIntError> {
+}
+impl Decrypt for String{
+    fn decrypt(&self, password: String) -> Result<String,E> {
+        let key_string = Pass_Encrypt::get_key(password);
+        let key = GenericArray::clone_from_slice(key_string.as_bytes());
+        let cipher = Aes128::new(&key);
+        let mut block = GenericArray::clone_from_slice(cypher_text.as_bytes());
 
-        let cipher = Aes128ECfb::new_from_slices(&self.key, &self.iv).unwrap();
-        let mut buf = cipher_text.to_vec();
-        let v_result = cipher.decrypt(&mut buf).unwrap();
-        let result = v_result.to_string();
+        cipher.decrypt_block(&mut block);
+        let s = block.to_vec();
+        let mut result = std::str::from_utf8(&s).unwrap().to_string();
         if (result.ends_with("verify")) {
-            let plain_text = result.truncate(result.len() - 6);
+            let plain_text = result.truncate(result.len() - 6).to_string();
             Ok(plain_text)
         }
         else {
             Err(E::new("error"))
         }
-    }*/
+    }
 }
+
+fn get_key(password: String) -> String {
+
+    let mut pw = password.clone();
+    while (pw.len() < 16){
+        pw += password.as_str();
+    }
+    pw
+}
+
+
