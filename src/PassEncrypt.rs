a3233e7f6eafde6431462ee22bbef44645d64fb7
foxjazz <fox21@foxjazz.net>
2022-12-16 07:03:42 -0700

    second

--- src/PassEncrypt.rs
+++ src/PassEncrypt.rs
@@ -1,27 +1,14 @@
-use aes::Aes256;
-use block_modes::{BlockMode, Cfb};
-use block_modes::block_padding::Pkcs7;
-use hex_literal::hex;
-use std::str;
-use std::env;
-use aes::Aes256;
-use block_modes::block_padding::Pkcs7;
-use block_modes::{BlockMode, Cbc};
-use sha2::Sha256;
 
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
-use std::io::Read;
-use std::num::ParseIntError;
-type Aes128ECfb = Cfb<Aes128, Pkcs7>;
+use aes::Aes128;
+use aes::cipher::{
+    BlockCipher, BlockEncrypt, BlockDecrypt, KeyInit,
+    generic_array::GenericArray,
+};
+
 pub struct Pass_Encrypt {
-    // ...
-    // Path: src\monitor\monitor.rs
-    iv: [i32;16]  ,
-    key: Vec<u8>,
-    // ...
+
 }
-fn generate_hash(password: &str) -> Vec<u8> {
+/*fn generate_hash(password: &str) -> Vec<u8> {
     // Create a new hasher
     let mut hasher = DefaultHasher::new();
 
@@ -37,36 +24,36 @@ fn generate_hash(password: &str) -> Vec<u8> {
 
     // Return the vector of bytes
     hash_bytes
-}
+}*/
 impl Pass_Encrypt{
-    pub fn new (password: &str) -> Pass_Encrypt {
+    /*pub fn new (password: &str) -> Pass_Encrypt {
         //let mut hasher = Sha256::digest::new();
 //        let key = generate_hash(password) ;
         let key = Sha256::digest(password.as_bytes());
         let iv = [0; 16];
         Pass_Encrypt {key, iv}
+    }*/
+
+    fn get_key(password: String) -> String {
+
+        let mut pw = password.clone();
+        while (pw.len() < 16){
+            pw += password.as_str();
+        }
+            pw
+    }
+    pub fn enc( plain_text : String, password: String) -> String {
+        let key_string = Pass_Encrypt::get_key(password);
+        let key = GenericArray::clone_from_slice(key_string.as_bytes());
+        let cipher = Aes128::new(&key);
+        let mut block = GenericArray::clone_from_slice(plain_text.as_bytes());
+
+        cipher.encrypt_block(&mut block);
+        let s = block.to_vec();
+        std::str::from_utf8(&s).unwrap().to_string()
 
     }
-    pub fn aes256(&self,plaintext: String) -> String {
-        let plaintext = plaintext + &"verify".to_string();
-        let cipher = Aes256::new(&self.key);
-        let mut encryptor = Cbc::new_var(&cipher, &self.iv).unwrap();
-        let mut buffer = plaintext.to_vec();
-        encryptor.encrypt(&mut buffer, plaintext.len() + 16, Pkcs7);
-        buffer.to_string()
-    }
-
-    pub fn enc(&self, plain_text : String) -> String {
-        let verify = plain_text + &"verify".to_string();
-
-        let cipher = Aes128ECfb::new_from_slices(&self.key, &self.iv).unwrap();
-        let pos = verify.len();
-        let mut buffer = [0u8; 128];
-        buffer[..pos].copy_from_slice(verify.as_bytes());
-        let ciphertext = cipher.encrypt(&mut buffer, pos).unwrap();
-        ciphertext.to_string()
-    }
-    pub fn dec(&self, cipher_text : String) -> Result<String,ParseIntError> {
+   /* pub fn dec(&self, cipher_text : String) -> Result<String,ParseIntError> {
 
         let cipher = Aes128ECfb::new_from_slices(&self.key, &self.iv).unwrap();
         let mut buf = cipher_text.to_vec();
@@ -79,5 +66,5 @@ impl Pass_Encrypt{
         else {
             Err(E::new("error"))
         }
-    }
+    }*/
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,10 @@
-mod PassEncrypt;
 
+mod PassEncrypt;
 use crate::PassEncrypt::Pass_Encrypt;
 
 fn main() {
     let stbe = "string to be endrypted".to_string();
-    let enc = Pass_Encrypt::new("password");
-    let cypher = enc.enc(stbe);
-    let result = enc.dec(cypher);
-    if Some(result) {
-        println!("result: {}", result);
-    }
-    else {
-        println!("error");
-    }
+
+    let cypher_txt = Pass_Encrypt::enc(stbe, "password".to_string());
+    println!("cipher_txt: {}", cypher_txt);
 }
